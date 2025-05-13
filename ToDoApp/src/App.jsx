8751507b9c27273c12bbd6b1559ed11c28462d9f
dinna21 <ransikadinura@gmail.com>
2025-05-13 12:45:08 +0530

    create the form element with create the logic for when we adding new items

--- ToDoApp/src/App.jsx
+++ ToDoApp/src/App.jsx
@@ -1,4 +1,61 @@
-export default function App()
-{
-  return "hi"
+import { useState } from 'react';
+import './style.css'
+
+export default function App() {
+  const [newItem,setNewItem] = useState("")
+  const [todos,setTodos] = useState([])
+
+  function HandleAddBtn(e)
+  {
+    e.preventDefault()
+
+    setTodos((currentTodos) =>{
+          return [
+          ...todos,{id: crypto.randomUUID(),title: newItem,completed: false},
+        ]
+    })
+  }
+  console.log(todos)
+  return (
+    <>
+      <form onSubmit={HandleAddBtn} className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input value={newItem}
+        onChange={e => setNewItem(e.target.value)}
+        type="text" id="item" />
+      </div>
+      <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox"/>Item1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+                <li>
+          <label>
+            <input type="checkbox"/>Item1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+                <li>
+          <label>
+            <input type="checkbox"/>Item1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+                <li>
+          <label>
+            <input type="checkbox"/>Item1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+
+
+  );
 }
